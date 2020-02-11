4f1546308f5c684732ee7b628963492992192ca9
Darek <dariusz.nitkowski@gmail.com>
2020-02-11 17:35:01 +0100

    Updates

---
+++ precache-manifest.17b2533c0e83ed687b4e48f089d13eb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab211dbeeca6e8960bb240c759a4de0",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "779486da1167e68f678b",
+    "url": "/React/static/css/main.e3f86c0c.chunk.css"
+  },
+  {
+    "revision": "53722eba1a761a2541ad",
+    "url": "/React/static/js/2.af18f193.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/React/static/js/2.af18f193.chunk.js.LICENSE"
+  },
+  {
+    "revision": "779486da1167e68f678b",
+    "url": "/React/static/js/main.6946ba62.chunk.js"
+  },
+  {
+    "revision": "db42a4d2e06e527cac3c",
+    "url": "/React/static/js/runtime-main.334448e5.js"
+  }
+]);
